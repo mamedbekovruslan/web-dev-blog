3f131db630f38e0035f88066c03f2668cb7eec60
mamedbekovRA <mamedbekovruslan@yandex.ru>
2024-12-01 17:10:35 +0300

    Add Header, Footer and Content

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Logo } from './components';
+
+const HeaderContainer = ({ className }) => (
+  <header className={className}>
+    <Logo />
+  </header>
+);
+
+export const Header = styled(HeaderContainer)`
+  position: fixed;
+  top: 0;
+  width: 1000px;
+  height: 120px;
+  padding: 20px 40px;
+  box-shadow: 0 -2px 17px #000;
+  background-color: #fff;
+`;
