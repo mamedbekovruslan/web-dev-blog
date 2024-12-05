9ff21adaa3ee0b17c8768fedb66003f16ae607c7
mamedbekovRA <mamedbekovruslan@yandex.ru>
2024-12-05 11:47:39 +0300

    Рефакторинг шапки

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
-import { Logo } from './components';
+import { ControlPanel, Logo } from './components';
+
+const Description = styled.div`
+  font-style: italic;
+`;
 
 const HeaderContainer = ({ className }) => (
   <header className={className}>
     <Logo />
+    <Description>
+      Веб технологии <br />
+      Написание кода <br />
+      Разбор ошибок <br />
+    </Description>
+    <ControlPanel />
   </header>
 );
 
 export const Header = styled(HeaderContainer)`
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   top: 0;
   width: 1000px;
