5b3bca7e62ce6d22e93b4ef0b340af7b8b14f5f2
mamedbekovRA <mamedbekovruslan@yandex.ru>
2024-12-15 16:20:30 +0300

    Рефакторинг авторизации

--- src/pages/authorization/authorization.js
+++ src/pages/authorization/authorization.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { server } from '../../bff';
 import { useState } from 'react';
-import { Button, Input } from '../../components';
+import { Button, H2, Input } from '../../components';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -11,18 +11,18 @@ const authFormSchema = yup.object().shape({
   login: yup
     .string()
     .required('Заполните логин')
-    .matches('/^\w+$/', 'Неверный заполнен логин. Допускаются только буквы или цифры')
-    .min(3, 'Неверный заполнен логин. Минимум 3 символа')
-    .max(15, 'Неверный заполнен логин. Максимум 15 символов'),
+    .matches(/^\w+$/, 'Неверно заполнен логин. Допускаются только буквы или цифры')
+    .min(3, 'Неверно заполнен логин. Минимум 3 символа')
+    .max(15, 'Неверно заполнен логин. Максимум 15 символов'),
   password: yup
     .string()
     .required('Заполните пароль')
     .matches(
-      '/^\w+$/',
-      'Неверный заполнен пароль. Допускаются только буквы, цифры и знаки # %',
+      /^\w+$/,
+      'Неверно заполнен пароль. Допускаются только буквы, цифры и знаки # %',
     )
-    .min(6, 'Неверный заполнен пароль. Минимум 6 символа')
-    .max(30, 'Неверный заполнен пароль. Максимум 30 символов'),
+    .min(6, 'Неверно заполнен пароль. Минимум 6 символа')
+    .max(30, 'Неверно заполнен пароль. Максимум 30 символов'),
 });
 
 const StyledLink = styled(Link)`
@@ -43,7 +43,7 @@ export const AuthorizationContainer = ({ className }) => {
   const {
     register,
     handleSubmit,
-    formState: { error },
+    formState: { errors },
   } = useForm({
     defaultValues: {
       login: '',
@@ -54,22 +54,28 @@ export const AuthorizationContainer = ({ className }) => {
 
   const [serverError, setServerError] = useState(null);
 
-  const onSubmit = ({ login, password }) => {
-    server.authorize(login, password).then((error, res) => {
+  const onSubmit = async ({ login, password }) => {
+    server.authorize(login, password).then(({ error, res }) => {
       if (error) {
         setServerError(`Ошибка запроса: ${error}`);
       }
     });
   };
 
-  const formError = error?.login?.message || error?.password?.message;
+  const formError = errors?.login?.message || errors?.password?.message;
   const errorMessage = formError || serverError;
 
   return (
     <div className={className}>
       <h2>Авторизация</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Input type="text" placeholder="Логин..." {...register('login')} />
+        <Input
+          type="text"
+          placeholder="Логин..."
+          {...register('login', {
+            onChange: () => setServerError(null),
+          })}
+        />
         <Input type="password" placeholder="Пароль..." {...register('password')} />
         <Button type="submit" disabled={!!formError}>
           Авторизация
