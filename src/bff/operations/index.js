2f2e41f06e61fe9d626ffdefa8af59c722910303
mamedbekov <mamedbekovruslan@yandex.ru>
2024-12-18 14:33:43 +0300

    Рефакторинг кода BFF

---
+++ src/bff/operations/index.js
@@ -0,0 +1,4 @@
+export * from './authorize';
+export * from './register';
+export * from './logout';
+export * from './fetch-roles';
--- src/pages/users/users.js
+++ src/pages/users/users.js
@@ -1,23 +1,60 @@
-import { H2 } from '../../components';
+import { Content, H2 } from '../../components';
 import { TableRow, UserRow } from './components';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useServerRequest } from '../../hooks';
 
 const UsersContainer = ({ className }) => {
-  const users = [];
+  const [users, setUsers] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const requestServer = useServerRequest();
+
+  useEffect(() => {
+    Promise.all([requestServer('fetchUsers'), requestServer('fetchRoles')]).then(
+      ([usersRes, rolesRes]) => {
+        if (usersRes.error || rolesRes.error) {
+          setErrorMessage(usersRes.error || rolesRes.error);
+          return;
+        }
+
+        setUsers(usersRes);
+        setRoles(rolesRes);
+      },
+    );
+
+    requestServer('fetchRoles').then(({ rolesError, res }) => {
+      if (rolesError) {
+        return;
+      }
+
+      setRoles(res);
+    });
+
+    requestServer('fetchUsers');
+  }, [requestServer]);
 
   return (
     <div className={className}>
-      <H2>Пользователи</H2>
-      <div>
-        <TableRow>
-          <div className="login-column">Логин</div>
-          <div className="registered-at-column">Дата регистрации</div>
-          <div className="role-column">Роль</div>
-        </TableRow>
-        {users.map(({ id, login, registeredAt, roleId }) => (
-          <UserRow key={id} login={login} registeredAt={registeredAt} roleId={roleId} />
-        ))}
-      </div>
+      <Content error={errorMessage}>
+        <H2>Пользователи</H2>
+        <div>
+          <TableRow>
+            <div className="login-column">Логин</div>
+            <div className="registered-at-column">Дата регистрации</div>
+            <div className="role-column">Роль</div>
+          </TableRow>
+          {users.map(({ id, login, registeredAt, roleId }) => (
+            <UserRow
+              key={id}
+              login={login}
+              registeredAt={registeredAt}
+              roleId={roleId}
+              roles={roles}
+            />
+          ))}
+        </div>
+      </Content>
     </div>
   );
 };
