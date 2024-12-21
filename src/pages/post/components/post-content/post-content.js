2f967a002e032a47af3aa4372d956c323a86b848
mamedbekovRA <mamedbekovruslan@yandex.ru>
2024-12-22 00:45:31 +0300

    Верстка поста

--- src/pages/post/components/post-content/post-content.js
+++ src/pages/post/components/post-content/post-content.js
@@ -1,17 +1,57 @@
 import styled from 'styled-components';
-import { H2 } from '../../../../components';
+import { H2, Icon } from '../../../../components';
 
 const PostContentContainer = ({
-  post: { className, id, title, imageUrl, content, publishedAt },
+  className,
+  post: { id, title, imageUrl, content, publishedAt },
 }) => {
   return (
     <div className={className}>
       <img src={imageUrl} alt={title} />
       <H2>{title}</H2>
-      <div className="special-panel">{publishedAt}</div>
-      <div>{content}</div>
+      <div className="special-panel">
+        <div className="published-at">
+          <Icon id="fa-calendar-o" size="18px" margin="0 10px 0 0" onClick={() => {}} />
+          {publishedAt}
+        </div>
+        <div className="buttons">
+          <Icon
+            id="fa-pencil-square-o"
+            size="21px"
+            margin="0 10px 0 0"
+            onClick={() => {}}
+          />
+          <Icon id="fa-trash-o" size="21px" onClick={() => {}} />
+        </div>
+      </div>
+      <div className="post-text">{content}</div>
     </div>
   );
 };
 
-export const PostContent = styled(PostContentContainer)``;
+export const PostContent = styled(PostContentContainer)`
+  & img {
+    float: left;
+    margin: 0 20px 10px 0;
+  }
+
+  & .special-panel {
+    display: flex;
+    justify-content: space-between;
+    margin: -20px 0 20px;
+  }
+
+  & .published-at {
+    display: flex;
+    font-size: 18px;
+  }
+
+  & i {
+    position: relative;
+    top: -1px;
+  }
+
+  & .buttons {
+    display: flex;
+  }
+`;
