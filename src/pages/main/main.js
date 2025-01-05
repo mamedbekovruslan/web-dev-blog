e7048bec585f7edd5c4ceaed566508197bb6b1ec
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-01-05 15:39:50 +0300

    Реализовать пагинацию

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,16 +2,24 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { PostCard } from './components/post-card/post-card';
 import { useServerRequest } from '../../hooks/use-server-request';
+import { Pagination } from './components';
+import { PAGINATION_LIMIT } from '../../bff/constants';
+import { getLastPageFromLinks } from './utils';
 
 const MainContainer = ({ className }) => {
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
   const requestServer = useServerRequest();
 
   useEffect(() => {
-    requestServer('fetchPosts').then((posts) => {
-      setPosts(posts.res);
-    });
-  }, [requestServer]);
+    requestServer('fetchPosts', page, PAGINATION_LIMIT).then(
+      ({ res: { posts, links } }) => {
+        setPosts(posts);
+        setLastPage(getLastPageFromLinks(links));
+      },
+    );
+  }, [requestServer, page]);
 
   return (
     <div className={className}>
@@ -27,6 +35,7 @@ const MainContainer = ({ className }) => {
           />
         ))}
       </div>
+      {lastPage > 1 && <Pagination page={page} lastPage={lastPage} setPage={setPage} />}
     </div>
   );
 };
